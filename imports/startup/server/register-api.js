bfe289499e0b9f7f8559cdf140db2b21a8760666
Yannick Panis <info@qucode.eu>
2018-02-26 20:33:22 +0100

    setup meteor production build process

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -9,10 +9,20 @@ import UserSchema from "../../api/user/user.graphql"
 import UserResolvers from "../../api/user/resolvers"
 
 // hiiiiiiiiiiiiiiii
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8000", "https://yannick-lernt.de", "https://naughty-jones-4790d1.netlify.com"]
 
 const corsOptions = {
-  origin: "http://localhost:8000",
-  optionsSuccessStatus: 200
+  origin: (origin, callback) => {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }
 const typeDefs = [JobSchema, UserSchema]
 const resolvers = merge(JobResolvers, UserResolvers)
